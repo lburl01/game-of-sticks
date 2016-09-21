1e1e6b53eb56e4aa6f3682703a29eec640ef1ea5
Lori Burleson <lori.burleson@gmail.com>
2016-09-20 20:14:33 -0400

    Normal 2v2 mode complete. README updated with playing instructions.

--- README.md
+++ README.md
@@ -5,4 +5,13 @@
 $ ruby sticks.rb
 ```
 
-**To Play:**
+**To Play:**  
+Don't be the person who has to take the last stick!
+
+The game will begin by asking for the number of total sticks you and your partner will have to whittle down until one stick remains. You may input a number between 10 and 100.
+
+Once your sticks are "thrown on the table" you and your opponent will alternate selecting whether you will remove 1, 2, or 3 sticks from the pile on the table. You will continue to do this until only one stick remains.
+
+The player who has to pick up the last stick loses!
+
+Good luck!
--- sticks.rb
+++ sticks.rb
@@ -84,23 +84,4 @@ class Sticks
     end
   end
 
-  # stick_count = sticks.validating_initial_sticks(sticks.total_stick_count)
-
-      # until stick_count == 0 # this is not right
-      #   puts "There are #{stick_count} sticks on the table."
-      #   print "Player 1, how many sticks will you pick up (1-3)? > "
-      #
-      #   # player_stick_choice = sticks.get_player_stick_choice
-      #   player_stick_choice = gets.chomp
-      #   player_sticks_choice_validate = sticks.validate_stick_choice(player_stick_choice)
-      #
-      #   if player_sticks_choice_validate == false
-      #     puts "Try entering 1, 2, or 3. > "
-      #   end
-      # end
-
-  def pick_up_sticks()
-
-  end
-
 end
